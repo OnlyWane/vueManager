0d656a1ab698be28630eff485cb157887b473926
OnlyWane <614096280@qq.com>
2019-01-13 15:00:25 +0800

    完成登录页及其基础验证

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,71 @@
 <template>
-  <div id="app">
-   
+  <div class="login-container">
+    <div class="form-container">
+      <el-form label-position="top" :rules="rules" ref="loginFormData" label-width="80px" :model="loginFormData">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="loginFormData.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="loginFormData.password"></el-input>
+        </el-form-item>
+        <el-button class="btn-login" type="primary" @click="submitForm('loginFormData')">主要按钮</el-button>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'app',
-  
-}
+  name: "app",
+  data() {
+    return {
+      loginFormData: {
+        username: "",
+        password: ""
+      },
+      rules: {
+          username: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'change' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 6, message: '密码长度不能小于6', trigger: 'change' }
+          ],
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            // console.log('error submit!!');
+            return false;
+          }
+        });
+      }
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss">
+.login-container {
+  height: 100%;
+  background-color: #333;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .form-container {
+    background-color: #fff;
+    width: 580px;
+    padding: 40px;
+    box-sizing: border-box;
+    border-radius: 10px;
+    .btn-login {
+      width: 100%;
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+//导入公共样式
+import './assets/base.css';
 
-Vue.config.productionTip = false
+// 导入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
+
+
+Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
 }).$mount('#app')
